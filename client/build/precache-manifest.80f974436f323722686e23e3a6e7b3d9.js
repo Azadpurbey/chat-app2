22a7c0d6fb775d6f2b2580da3473ec4eea3635f1
Azad Kumar <azadpurbey2@gmail.com>
2021-03-09 23:17:40 +0530

    git ignore changes

---
+++ client/build/precache-manifest.80f974436f323722686e23e3a6e7b3d9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a8c7d17c3dcb2d8591cbb37904477f47",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5ed806c0256a9f7b379e",
+    "url": "/static/css/main.8f1a14d2.chunk.css"
+  },
+  {
+    "revision": "bd7ec9ee17e09919a95e",
+    "url": "/static/js/2.249e7d94.chunk.js"
+  },
+  {
+    "revision": "5ed806c0256a9f7b379e",
+    "url": "/static/js/main.3c52a305.chunk.js"
+  },
+  {
+    "revision": "98e8e9e35f628d408499",
+    "url": "/static/js/runtime~main.190f912a.js"
+  }
+]);
